964a6a6fceb4b4080fd8981ab2ad81d2217aa5c6
CandiceCishe1 <15320863@mylife.unisa.ac.za>
2023-04-27 18:54:45 -0700

    Finds the largest of 3 numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,33 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-		if (b > c)
-			largest = a;
-		else
-		{
-			if (a > c)
-				largest = a;
-			else
-				largest = c;
-		}
+		largest = a;
 	}
-	else if (b > a)
+	else if (b > a && b > c)
 	{
-		if (a > c)
-			largest = b;
-		else
-		{
-			if (b > c)
-				largest = b;
-			else
-				largest = c;
-		}
+		largest = b;
 	}
 	else if (c > a && c > b)
+	{
 		largest = c;
-
+	}
 	return (largest);
 
 }
