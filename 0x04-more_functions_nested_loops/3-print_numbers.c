8630571236bec911767348a2bd5b429af60dd2f5
CandiceCishe1 <15320863@mylife.unisa.ac.za>
2023-04-29 02:22:03 -0700

    Code that prints numbers 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Function that prints the 1 to 9 digits
+ */
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+
+}
