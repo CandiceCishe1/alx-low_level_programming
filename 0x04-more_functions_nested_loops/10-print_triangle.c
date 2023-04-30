56daa5b53cd55d32fce298b7e789b7a13c57d2e6
CandiceCishe1 <15320863@mylife.unisa.ac.za>
2023-04-30 05:03:02 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size: the value to be computed
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int spaces;
+	int i;
+	int row;
+	int y = 35;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (spaces = size - row; spaces >= 1; spaces--)
+		{
+			_putchar(' ')"
+		}
+		for (i = 1; i <= row; i++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
