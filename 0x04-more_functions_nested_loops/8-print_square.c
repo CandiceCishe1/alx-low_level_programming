9fcc39c6250fe6ba481a1e21c75faf2998846931
CandiceCishe1 <15320863@mylife.unisa.ac.za>
2023-04-30 00:56:23 -0700

    function that prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,21 @@
  */
 void print_square(int size)
 {
+	int n;
 	int i;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 
 
-	for (i = 0; i <= size; i++)
-		_putchar(35);
-	_putchar('\n');
+	for (n = 1; n <= size; n++)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
 }
