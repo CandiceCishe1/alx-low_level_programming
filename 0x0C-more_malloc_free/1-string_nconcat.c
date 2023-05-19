415501b3cc23474851bac123d7a36befc7c7a97a
CandiceCishe1 <15320863@mylife.unisa.ac.za>
2023-05-18 20:59:44 -0700

    Read

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function to concatnate strings with n bytes
+ * @s1: destination for concatnation
+ * @s2: source of string
+ * @n: int type for size of byte
+ * Return: pointer to new memory allocated
+ */
